1229793a4d3aee609f8ab61a568447fcd2e7784d
Siri-Pulakanti <pulakantisiri1@gmail.com>
2025-05-27 18:03:07 +0530

    added css functionalities

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <>
+        <Sidebar />
+      </>
+    </div>
+  );
 }
 
 export default App;
